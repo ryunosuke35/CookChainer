2736bcacc708d5d6e2bcd85b5d0f6b87c2bd99be
ryunosuke35 <6bev2103@gmail.com>
2021-07-30 23:38:08 +0900

    add: Postテーブルの作成 #1

--- db/migrate/20210730142623_create_posts.rb
+++ db/migrate/20210730142623_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :name, null: false
+      t.text :url
+      t.string :memo
+      t.text :image
 
       t.timestamps
     end

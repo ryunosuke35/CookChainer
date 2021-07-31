705b9569737058c1beae9fcc1fee27fa03071acd
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 23:30:03 +0900

    fix: postを投稿できないバグを修正 #9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_to posts_path, notice: "Post was successfully created."
     else

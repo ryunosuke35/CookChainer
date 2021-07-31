1d67e66a720bd1953c2956732164c8fc4bc005f9
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 14:27:19 +0900

    add: postとcategoryのアソシエーションの設定 #5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+
+  has_many :post_categories, dependent: :destroy
+  has_many :categories, through: :post_categories
 end

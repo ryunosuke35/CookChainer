7f264c12373185506036abb032e72a6165a37c57
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 10:51:42 +0900

    add: tagのモデルとコントローラーの作成 #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_142623) do
+ActiveRecord::Schema.define(version: 2021_07_31_015010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2021_07_30_142623) do
     t.text "url"
     t.string "memo"
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

1e26d3810a25cb388de158861eb9c4c88d70627e
ryunosuke35 <6bev2103@gmail.com>
2021-08-01 00:57:48 +0900

    add: tag_categoryテーブルの作成 #11

---
+++ db/migrate/20210731153727_create_tag_categories.rb
@@ -0,0 +1,9 @@
+class CreateTagCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tag_categories do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_141550) do
+ActiveRecord::Schema.define(version: 2021_07_31_153727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 2021_07_31_141550) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tag_categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|

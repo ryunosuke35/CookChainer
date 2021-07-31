9ef9c976004d24f3fe471779feb276d70cac52f0
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 11:25:03 +0900

    add: postとtagの中間テーブルの作成(post_tag) #2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_015010) do
+ActiveRecord::Schema.define(version: 2021_07_31_022104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "post_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_07_31_015010) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
 end

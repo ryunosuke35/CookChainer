13de55eba743c4ef2a46bab0b573c7f232008851
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 19:23:27 +0900

    add: deviseのコントローラーを自分のプロジェクト内に生成 #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+
   resources :posts
   resources :tags
   resources :categories
   root 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
 end

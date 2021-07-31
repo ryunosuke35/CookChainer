dc6e668b039e035f795495d3d5f1295059df701c
ryunosuke35 <6bev2103@gmail.com>
2021-07-31 23:00:23 +0900

    add: postとuserのアソシエーションの設定 #9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
-  
+
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end

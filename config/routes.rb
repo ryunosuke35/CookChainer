f04acfea7d7f44986e8508081c3d12d45bffcca8
ryunosuke35 <6bev2103@gmail.com>
2021-08-05 18:54:44 +0900

    update: リファクタリング #41

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
 
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions'
-  # }
+  root 'posts#index'
 
   resources :posts do
     collection do
       get :index2
     end
   end
+
+  devise_for :users
+
   resources :tags
   resources :categories
-  resources :favorites, only: [:create, :destroy, :index]
   resources :tag_categories
+  resources :favorites, only: [:create, :destroy, :index]
   resources :users, only: [:show]
-  
-  root 'posts#index'
 
 end

0610a994b8c59a49166f567c793a3a0ba7e79b83
ryunosuke35 <6bev2103@gmail.com>
2021-08-05 18:50:32 +0900

    update: signup signin 後にマイページに遷移するように設定 #41

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
 
   def show
-  end
-
-
-  private
-
-  def set_user
     @user = User.find(params[:id])
   end
 

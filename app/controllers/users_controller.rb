f79c36efeb49b9daa27b45c7b9e710c070ed0ce4
ryunosuke35 <6bev2103@gmail.com>
2021-08-05 20:35:25 +0900

    add: デフォルトのアイコン画像を登録 #41

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
   end
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    binding.pry
     if @user.update(user_params)
       redirect_to @user, notice: "Tag was successfully updated."
     else
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image, :password, :password_confirmation, :profile)
   end
 
 end

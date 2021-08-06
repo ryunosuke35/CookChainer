9fa85d7c24c5d7aa61176a8e2ba18ae677c803bd
ryunosuke35 <6bev2103@gmail.com>
2021-08-06 21:38:28 +0900

    add: categoryにバリデーションを設定 #46

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 1..30 }
+
   has_many :post_categories, dependent: :destroy
   has_many :posts, through: :post_categories
 end

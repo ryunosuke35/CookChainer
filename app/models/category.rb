e05e85274bd3e7ec7e2845a10809bd584cdb35cb
ryunosuke35 <6bev2103@gmail.com>
2021-08-06 22:04:50 +0900

    add: tag_categoryバリデーションを設定 #46

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { in: 1..30 }
+  validates :name, presence: true, uniqueness: true, length: { in: 1..50 }
 
   has_many :post_categories, dependent: :destroy
   has_many :posts, through: :post_categories
--- app/models/tag_category.rb
+++ app/models/tag_category.rb
@@ -1,3 +1,5 @@
 class TagCategory < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 1..50 }
+
   has_many :tags
 end

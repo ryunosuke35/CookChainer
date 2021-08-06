bc7ecea5312676079285aec51e751a243e55a3bc
ryunosuke35 <6bev2103@gmail.com>
2021-08-06 22:23:27 +0900

    add: tagのバリデーションを設定 #46

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 1..50 }
+
   has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
 

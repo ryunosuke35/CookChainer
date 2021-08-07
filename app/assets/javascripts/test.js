da64d45b7b439a05e0878248c5b769959a3ebff4
ryunosuke35 <6bev2103@gmail.com>
2021-08-07 19:02:58 +0900

    fix: プルダウンのリンクから遷移した画面でjsが動かないバグを修正 #31 , data: {"turbolinks" => false} を削除 appication.jsの //= require turbolinks を削除

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,6 +1,7 @@
 $(function() {
-  $('accordion dd').hide();
+  $('#accordion dd').hide();
   $('#accordion dt').on('click', function() {
     $(this).next('dd').slideToggle('fast');
   });
+  $('#accordion dd').hide();
 });

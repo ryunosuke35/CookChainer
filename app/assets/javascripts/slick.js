c0b59885615960ab70a90d9a8ffee9694b50154d
ryunosuke35 <6bev2103@gmail.com>
2021-08-12 11:44:20 +0900

    add: slickでスライダーを作成 #55

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    dots: true,
+    infinite: true,
+    pauseOnHover: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+  $('.slick-dots li').on('mouseover', function() {
+  $('.slider').slick('goTo', $(this).index());
+  });
+});

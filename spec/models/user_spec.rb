caadb773c964695091c30d329b3d7c9fbeb66fc6
ryunosuke35 <6bev2103@gmail.com>
2021-08-15 14:44:08 +0900

    update: userの愛でるスペックの修正 #63

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,60 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :model do
-
-  context '名前、メールアドレス、パスワード、確認用パスワードが入力された場合' do
-    it "バリデーションが通る" do
-      user = FactoryBot.build(:user)
-      expect(user).to be_valid
+  describe 'バリデーションのテスト' do
+    context '名前、メールアドレス、パスワード、確認用パスワードが入力された場合' do
+      it "バリデーションが通る" do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+      end
     end
-  end
-  context '名前が空の場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, name: nil)
-      expect(user).not_to be_valid
+    context '名前が空の場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, name: nil)
+        expect(user).not_to be_valid
+      end
     end
-  end
-  context 'メールアドレスが空の場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, email: nil)
-      expect(user).not_to be_valid
+    context 'メールアドレスが空の場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, email: nil)
+        expect(user).not_to be_valid
+      end
     end
-  end
-  context 'メールアドレスが重複している場合' do
-    it "バリデーションにひっかかる" do
-      user1 = FactoryBot.create(:user)
-      user2 = FactoryBot.build(:user)
-      expect(user2).not_to be_valid
+    context 'メールアドレスが重複している場合' do
+      it "バリデーションにひっかかる" do
+        user1 = FactoryBot.create(:user)
+        user2 = FactoryBot.build(:user)
+        expect(user2).not_to be_valid
+      end
     end
-  end
-  context 'メールアドレスが不正な値の場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, email: 'aaaaaaaaaaaaaaaaa' )
-      expect(user).not_to be_valid
+    context 'メールアドレスが不正な値の場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, email: 'aaaaaaaaaaaaaaaaa' )
+        expect(user).not_to be_valid
+      end
     end
-  end
-  context 'メールアドレスを正規表現と比較した場合' do
-    it "マッチする" do
-      user = FactoryBot.build(:user)
-      expect(user[:email]).to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+    context 'メールアドレスを正規表現と比較した場合' do
+      it "マッチする" do
+        user = FactoryBot.build(:user)
+        expect(user[:email]).to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+      end
     end
-  end
-  context 'パスワードが空の場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, password: nil)
-      expect(user).not_to be_valid
+    context 'パスワードが空の場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, password: nil)
+        expect(user).not_to be_valid
+      end
     end
-  end
-  context 'パスワードが6文字未満の場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, password: 'passw' )
-      expect(user).not_to be_valid
+    context 'パスワードが6文字未満の場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, password: 'passw' )
+        expect(user).not_to be_valid
+      end
     end
-  end
-  context 'パスワードと確認用パスワードの内容が異なる場合' do
-    it "バリデーションにひっかかる" do
-      user = FactoryBot.build(:user, password: 'password', password_confirmation: 'passwords' )
-      expect(user).not_to be_valid
+    context 'パスワードと確認用パスワードの内容が異なる場合' do
+      it "バリデーションにひっかかる" do
+        user = FactoryBot.build(:user, password: 'password', password_confirmation: 'passwords' )
+        expect(user).not_to be_valid
+      end
     end
   end
 end
---
+++ spec/system/user_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe 'ユーザ登録・セッション機能・管理画面のテスト', type: :system do
+end

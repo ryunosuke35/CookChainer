d08584f6db1120c53750cd4d7f11fcefd8ba09c5
ryunosuke35 <6bev2103@gmail.com>
2021-08-15 17:53:16 +0900

    add: アカウント登録機能のテストを実装 #63

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  skip_before_action :login_required, only: [:new]
+  skip_before_action :login_required, only: [:new, :create]
 
 
   # GET /resource/sign_up
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -2,24 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'User', type: :system do
 
-  describe 'アカウント登録のテスト' do
-    context 'アカウント登録をした場合' do
+  # describe 'アカウント登録機能のテスト' do
+  #   context 'アカウント登録をした場合' do
+  #     it 'マイページが表示される' do
+  #       visit new_user_registration_path
+  #       fill_in 'user_name', with: '田中太郎'
+  #       fill_in 'user_email', with: 'tanaka@gmail.com'
+  #       fill_in 'user_password', with: 'password'
+  #       fill_in 'user_password_confirmation', with: 'password'
+  #       click_on 'アカウント登録'
+  #       expect(page).to have_content 'アカウント登録が完了しました。'
+  #       expect(page).to have_content '田中太郎'
+  #       expect(page).to have_content 'tanaka@gmail.com'
+  #     end
+  #   end
+  # end
+
+  describe 'ログイン機能のテスト' do
+    context 'ログインをした場合' do
       it 'マイページが表示される' do
-        visit new_user_registration_path
+        visit new_user_session_path
         fill_in 'user_name', with: '田中太郎'
         fill_in 'user_email', with: 'tanaka@gmail.com'
         fill_in 'user_password', with: 'password'
-        fill_in 'user_password_confirmation', with: 'password'
-        click_on '登録する'
-        expect(page).to have_content 'アカウント登録が完了しました。'
+        click_on 'ログイン'
+        expect(page).to have_content 'ログインしました。'
         expect(page).to have_content '田中太郎'
-        expect(page).to have_content 'matsumura@gmail.com'
+        expect(page).to have_content 'tanaka@gmail.com'
       end
     end
-    context 'ユーザがログインせずタスク一覧画面にとぼうとした場合' do
+    context 'ログアウトをした場合' do
       it 'ログイン画面に遷移する' do
-        visit tasks_path
-        expect(page).to have_content 'ログイン'
+        visit new_user_session_path
+        fill_in 'user_name', with: '田中太郎'
+        fill_in 'user_email', with: 'tanaka@gmail.com'
+        fill_in 'user_password', with: 'password'
+        click_on 'ログイン'
+        expect(page).to have_content 'ログインしました。'
+        expect(page).to have_content '田中太郎'
+        expect(page).to have_content 'tanaka@gmail.com'
       end
     end
   end

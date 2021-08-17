7c765224614401bc661a5a04fa2e43a341cff49b
ryunosuke35 <6bev2103@gmail.com>
2021-08-17 16:43:34 +0900

    fix: タグの作成、編集画面で、カテゴリーの選択方法をラジオボタンに変更 #63

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -15,19 +15,12 @@ class TagsController < ApplicationController
   end
 
   def create
-    if tag_params[:tag_category_id].reject(&:blank?).count < 2
-      @tag = Tag.new(tag_params)
-      @tag.tag_category_id = tag_params[:tag_category_id][1]
-
-      if @tag.save
-        redirect_to tags_path, notice: "「#{tag_params[:name]}」を作成しました"
-      else
-        render :new, status: :unprocessable_entity
-      end
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to tags_path, notice: "「#{tag_params[:name]}」を作成しました"
     else
-      redirect_to new_tag_path, notice: "カテゴリーを1つ選択してください"
+      render :new, status: :unprocessable_entity
     end
-
   end
 
   def update

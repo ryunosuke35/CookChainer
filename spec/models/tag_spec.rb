02d48ccfb16a750f9d4125dd8582af202f62205a
ryunosuke35 <6bev2103@gmail.com>
2021-08-20 21:46:17 +0900

    テストの最終チェック #68

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Tag', type: :model do
 
+
+
   context 'タグ名が入力された場合' do
     it "バリデーションが通る" do
-      tag = FactoryBot.build(:tag)
+      tag_category = FactoryBot.create(:tag_category)
+      tag = FactoryBot.build(:tag, tag_category:tag_category)
       expect(tag).to be_valid
     end
   end
@@ -16,8 +19,9 @@ RSpec.describe 'Tag', type: :model do
   end
   context 'タグ名が重複している場合' do
     it "バリデーションにひっかかる" do
-      tag1 = FactoryBot.create(:tag)
-      tag2 = FactoryBot.build(:tag)
+      tag_category = FactoryBot.create(:tag_category)
+      tag1 = FactoryBot.create(:tag, tag_category:tag_category)
+      tag2 = FactoryBot.build(:tag, tag_category:tag_category)
       expect(tag2).not_to be_valid
     end
   end
